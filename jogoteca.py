5ff70ed40e783208fe6bacd06fc9a3129f40e152
Ana Carolina <anacarolina.ifmgsabara@gmail.com>
2022-07-06 11:59:04 -0300

    meu primeiro git

---
+++ jogoteca.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+class jogo:
+
+    def __init__ (self,nome,categoria,console):
+        self.nome =nome
+        self.categoria=categoria
+        self.console= console
+
+jogo1= jogo('God of War','ação','playstation')
+jogo2=jogo('CS:GO','Tiro','PC')
+jogo3=jogo('Minecraft', 'construção','PC')
+
+
+app = Flask(__name__)
+
+lista = [jogo1, jogo2, jogo3]
+
+@app.route('/inicio')
+def ola():
+    return render_template('lista.html',titulo='Meus Jogos', jogos=lista)
+
+app.run()
