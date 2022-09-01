e971b8669ecd31621d12312af539d310cdb1e8cf
Ana Carolina Carolina <108819417+anacarolina1826@users.noreply.github.com>
2022-09-01 13:11:39 +0000

    criação de  templetes: Base,login. Criar programa usuario.py

--- ven/main.py
+++ ven/main.py
@@ -38,3 +38,22 @@ def criar():
 
 app.run(host='0.0.0.0', port=81, debug=True)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    email = request.form['email']
+    senha = request.form['senha']
+    usuario = usuarios.buscar(email, senha)
+    if usuario is None:
+      flash('Usuário/senha inválidos.')
+    else:
+      session['usuario_email'] = usuario.email
+      session['usuario_nome'] = usuario.nome
+      return render_template('login.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+  session.pop('usuario_email', None)
+  session.pop('usuario_nome', None)
+  return redirect(url_for('index')) 
+
