1ec4e492622db1f07d0e026beda765b9d20c5fee
Ana Carolina Carolina <108819417+anacarolina1826@users.noreply.github.com>
2022-09-22 02:07:32 +0000

    atualização do estilo, main.py e base.html

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from flask import Flask, render_template, request, redirect,session,flash,url_for,abort
-import usuarios
+from flask import Flask, render_template, request, redirect,session,flash, url_for,abort,g
+#import usuarios
 
-class jogo:
 
-    def __init__ (self,nome,categoria,console):
-        self.nome =nome
-        self.categoria=categoria
-        self.console= console
 
-jogo1= jogo('God of War','ação','playstation')
-jogo2=jogo('CS:GO','Tiro','PC')
-jogo3=jogo('Minecraft', 'construção','PC')
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+
+jogo1= Jogo('God of War','ação','playstation')
+jogo2= Jogo('CS:GO','Tiro','PC')
+lista = [jogo1, jogo2]
 
 
 app = Flask(__name__)
 app.secrety_key='ifmg'
 
-lista = [jogo1, jogo2, jogo3]
+
 #exibir a tabela com meus jogos
 @app.route('/')
 def ola():
@@ -26,17 +29,22 @@ def ola():
   #rota: Exibir o formulario para cadstro de novo jogo
 @app.route('/novo')
 def novo():
+  if not usuario_logado():
+    abort(403)
   return render_template('novo.html')
 
   
 
 @app.route('/criar', methods=['POST',])
 def criar():
+  if not usuario_logado():
+    abort(403)
+  return render_template('novo.html')
   nome = request.form['nome']
   categoria = request.form['categoria']
   console = request.form['console']
   
-  novo_jogo = jogo(nome, categoria, console)
+  jogo = Jogo(nome, categoria, console)
   lista.append(novo_jogo)
   
   return render_template('lista.html',titulo='Meus Jogos', jogos=lista)
@@ -46,6 +54,7 @@ def criar():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
+    
     email = request.form['email']
     senha = request.form['senha']
     usuario = usuarios.buscar(email, senha)
@@ -64,6 +73,9 @@ def logout():
   session.pop('usuario_nome', None)
   return redirect(url_for('index')) 
 
+  
+#proteger a rota da pagina novo.html para que apenas usuarios credeciados(logados) acessem a pagina para criar novos jogos
+#a função abaixo verfica se usuario está logado.
 def usuario_logado():
     return 'usuario_email' in session
 
